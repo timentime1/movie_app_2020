d44f5992478bfde2490c5448e1022174a65f59a5
timentime1 <odh082@daum.net>
2020-10-12 16:53:22 +0900

    Updates

---
+++ precache-manifest.aa2ecb5a59d5cfdd98c13d5b50891300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b106a5536bff3184afebcc239216aa5a",
+    "url": "/ovie_app_2020/index.html"
+  },
+  {
+    "revision": "f442ff8b13d4e6203ec0",
+    "url": "/ovie_app_2020/static/css/main.6d61d015.chunk.css"
+  },
+  {
+    "revision": "4c72688f768eb1abb0e0",
+    "url": "/ovie_app_2020/static/js/2.69a0b1d1.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/ovie_app_2020/static/js/2.69a0b1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f442ff8b13d4e6203ec0",
+    "url": "/ovie_app_2020/static/js/main.eaf2cb83.chunk.js"
+  },
+  {
+    "revision": "f6773b6fc06f63a870a7",
+    "url": "/ovie_app_2020/static/js/runtime-main.58fd6bad.js"
+  }
+]);
